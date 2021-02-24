997ab12c26c69d557d8aae956ef112402eb120ba
rapkeb <48759641+rapkeb@users.noreply.github.com>
2021-02-24 13:51:07 +0200

    Add files via upload

---
+++ Ex3.3/SimpleCompiler/Compiler.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCompiler
+{
+    public class Compiler
+    {
+
+
+        public Compiler()
+        {
+
+        }
+
+
+        public List<VarDeclaration> ParseVarDeclarations(List<string> lVarLines)
+        {
+            List<VarDeclaration> lVars = new List<VarDeclaration>();
+            for(int i = 0; i < lVarLines.Count; i++)
+            {
+                List<Token> lTokens = Tokenize(lVarLines[i], i);
+                TokensStack stack = new TokensStack(lTokens);
+                VarDeclaration var = new VarDeclaration();
+                var.Parse(stack);
+                lVars.Add(var);
+            }
+            return lVars;
+        }
+
+
+        public List<LetStatement> ParseAssignments(List<string> lLines)
+        {
+            List<LetStatement> lParsed = new List<LetStatement>();
+            List<Token> lTokens = Tokenize(lLines);
+            TokensStack sTokens = new TokensStack();
+            for (int i = lTokens.Count - 1; i >= 0; i--)
+                sTokens.Push(lTokens[i]);
+            while(sTokens.Count > 0)
+            {
+                LetStatement ls = new LetStatement();
+                ls.Parse(sTokens);
+                lParsed.Add(ls);
+
+            }
+            return lParsed;
+        }
+
+ 
+
+        public List<string> GenerateCode(LetStatement aSimple, Dictionary<string, int> dSymbolTable)
+        {
+            List<string> lAssembly = new List<string>();
+            //add here code for computing a single let statement containing only a simple expression
+            return lAssembly;
+        }
+
+
+        public Dictionary<string, int> ComputeSymbolTable(List<VarDeclaration> lDeclerations)
+        {
+            Dictionary<string, int> dTable = new Dictionary<string, int>();
+            //add here code to comptue a symbol table for the given var declarations
+            //real vars should come before (lower indexes) than artificial vars (starting with _), and their indexes must be by order of appearance.
+            //for example, given the declarations:
+            //var int x;
+            //var int _1;
+            //var int y;
+            //the resulting table should be x=0,y=1,_1=2
+            //throw an exception if a var with the same name is defined more than once
+            return dTable;
+        }
+
+
+        public List<string> GenerateCode(List<LetStatement> lSimpleAssignments, List<VarDeclaration> lVars)
+        {
+            List<string> lAssembly = new List<string>();
+            Dictionary<string, int> dSymbolTable = ComputeSymbolTable(lVars);
+            foreach (LetStatement aSimple in lSimpleAssignments)
+                lAssembly.AddRange(GenerateCode(aSimple, dSymbolTable));
+            return lAssembly;
+        }
+
+        public List<LetStatement> SimplifyExpressions(LetStatement s, List<VarDeclaration> lVars)
+        {
+            //add here code to simply expressins in a statement. 
+            //add var declarations for artificial variables.
+            return null;
+        }
+        public List<LetStatement> SimplifyExpressions(List<LetStatement> ls, List<VarDeclaration> lVars)
+        {
+            List<LetStatement> lSimplified = new List<LetStatement>();
+            foreach (LetStatement s in ls)
+                lSimplified.AddRange(SimplifyExpressions(s, lVars));
+            return lSimplified;
+        }
+
+ 
+        public LetStatement ParseStatement(List<Token> lTokens)
+        {
+            TokensStack sTokens = new TokensStack();
+            for (int i = lTokens.Count - 1; i >= 0; i--)
+                sTokens.Push(lTokens[i]);
+            LetStatement s = new LetStatement();
+            s.Parse(sTokens);
+            return s;
+        }
+
+ 
+        public List<Token> Tokenize(string sLine, int iLine)
+        {
+            //insert here code from assignment 3.1 to tokenize a single line
+            return null;
+        }
+
+        public List<Token> Tokenize(List<string> lCodeLines)
+        {
+            List<Token> lTokens = new List<Token>();
+            for (int i = 0; i < lCodeLines.Count; i++)
+            {
+                string sLine = lCodeLines[i];
+                List<Token> lLineTokens = Tokenize(sLine, i);
+                lTokens.AddRange(lLineTokens);
+            }
+            return lTokens;
+        }
+
+    }
+}
